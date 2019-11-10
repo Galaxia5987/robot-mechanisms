86249bf734c76a9bbc1e10667e49996f7c29ea87
Adam <adam.vichik@gmail.com>
2019-11-10 17:27:25 +0200

    Create Wrist subsystem class

---
+++ src/main/java/robot/subsystems/wrist/Wrist.java
@@ -0,0 +1,15 @@
+package robot.subsystems.wrist;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Wrist extends Subsystem {
+
+    public Wrist() {
+        
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
