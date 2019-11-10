0532dc570b44aefd8e9f7a71a2ddf8e68620439f
Adam <adam.vichik@gmail.com>
2019-11-10 17:57:28 +0200

    Configure kP, kI, kD

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -19,6 +19,11 @@ public class Constants {
     }
 
     public static class Wrist {}
+        public static final int TALON_PID_SLOT = 0;
+        public static final double KP = 0;
+        public static final double KI = 0;
+        public static final double KD = 0;
+        public static final int TALON_TIMEOUT_MS = 20;
 
     public static class ExampleSubsystem1 {
         //All of the Subsystem specific constants go here,and need to be static.
--- src/main/java/robot/subsystems/wrist/Wrist.java
+++ src/main/java/robot/subsystems/wrist/Wrist.java
@@ -3,12 +3,17 @@ package robot.subsystems.wrist;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import static robot.Constants.Wrist.*;
 import static robot.Ports.Wrist.MOTOR;
 
 public class Wrist extends Subsystem {
     private TalonSRX armMotor = new TalonSRX(MOTOR);
 
     public Wrist() {
+        armMotor.config_kP(TALON_PID_SLOT, KP, TALON_TIMEOUT_MS);
+        armMotor.config_kI(TALON_PID_SLOT, KI, TALON_TIMEOUT_MS);
+        armMotor.config_kD(TALON_PID_SLOT, KD, TALON_TIMEOUT_MS);
+    }
         
     }
 
