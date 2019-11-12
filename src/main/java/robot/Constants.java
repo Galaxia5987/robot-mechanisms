c1db9b3a039d2c78580db5ae07a0698a91bfd329
Adam <adam.vichik@gmail.com>
2019-11-12 19:35:42 +0200

    Create KF constant

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
         public static final double KP = 0;
         public static final double KI = 0;
         public static final double KD = 0;
+        public static final double KF = 0;
         public static final int TIMEOUT_MS= 0;
         public static final boolean IS_MASTER_INVERTED = false;
         public static final boolean IS_SLAVE_INVERTED = false;
