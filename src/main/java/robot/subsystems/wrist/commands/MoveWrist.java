22bede5b173ba62c334ffa2b147596b2d7539cfa
Adam <adam.vichik@gmail.com>
2019-11-12 17:02:30 +0200

    Add setAngle() to the MoveWrist Command's initialize

--- src/main/java/robot/subsystems/wrist/commands/MoveWrist.java
+++ src/main/java/robot/subsystems/wrist/commands/MoveWrist.java
@@ -17,6 +17,7 @@ public class MoveWrist extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        wrist.setAngle(angle);
     }
 
     // Called repeatedly when this Command is scheduled to run
