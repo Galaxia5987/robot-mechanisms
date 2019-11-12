2c0086e2f991692448bcfc7a39c4cc4c808962be
Adam <adam.vichik@gmail.com>
2019-11-12 17:13:27 +0200

    Add update() and reset() methods to MoveWrist Command

--- src/main/java/robot/subsystems/wrist/commands/MoveWrist.java
+++ src/main/java/robot/subsystems/wrist/commands/MoveWrist.java
@@ -18,11 +18,13 @@ public class MoveWrist extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        wrist.reset();
         wrist.setAngle(angle);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        wrist.update();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -32,5 +34,6 @@ public class MoveWrist extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+
     }
 }
