c36d9ab7f3a3e4d9a779b8439cc85cbb332bf09b
Adam <adam.vichik@gmail.com>
2019-11-12 19:36:17 +0200

    Create MAX_CURRENT constant

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -27,6 +27,7 @@ public class Constants {
         public static final int TIMEOUT_MS= 0;
         public static final boolean IS_MASTER_INVERTED = false;
         public static final boolean IS_SLAVE_INVERTED = false;
+        public static final int MAX_CURRENT = 35;
     }
     public static class ExampleSubsystem1 {
         //All of the Subsystem specific constants go here,and need to be static.
