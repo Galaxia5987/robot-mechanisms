c944b466bee0c6d7e0bb521bbf70684976752d28
Adam <adam.vichik@gmail.com>
2019-11-17 17:46:08 +0200

    Fix Shooter ports

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -7,7 +7,7 @@ package robot;
  */
 public class Ports {
     public static class Shooter {
-        public static final int MASTER = 1;
-        public static final int SLAVE = 2;
+        public static final int MASTER = 11;
+        public static final int SLAVE = 12;
     }
 }
