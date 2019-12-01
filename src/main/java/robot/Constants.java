6c7c9e01cd3f5f7855c399b00598ff7175e55160
Barel Shlidor <barel@shlidor.org>
2019-12-01 18:45:45 +0200

    Switch motor ports

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -25,8 +25,8 @@ public class Constants {
         public static final double KD = 0.31;
         public static final double KF = 0;
         public static final int TIMEOUT_MS = 20;
-        public static final boolean IS_MASTER_INVERTED = true;
-        public static final boolean IS_SLAVE_INVERTED = false;
+        public static final boolean IS_MASTER_INVERTED = false;
+        public static final boolean IS_SLAVE_INVERTED = true;
         public static final int MAX_CURRENT = 35; //[A]
         public static final double TICKS_PER_METER = 36 / (2 * 0.05 * Math.PI);
         public static final double TICKS_PER_ROTATION = 36;
@@ -34,7 +34,7 @@ public class Constants {
         public static final double ANGLE = 45; // [deg]
         public static final double RADIUS = 0.05; // [m]
         public static final double g = 9.80665; // [m/sec^2]
-        public static final double TARGET_DISTANCE = 2;
+        public static final double TARGET_DISTANCE = 2; // [m]
         public static final double SHOOTING_TIME = 3.5;
     }
 
