408e105f20e11319f12fc9b33deec5b0bc305f3f
Barel <Barel@shlidor.org>
2019-12-24 17:44:10 +0200

    Implement Turret Command class

--- src/main/java/robot/subsystems/turret/commands/TurnTurret.java
+++ src/main/java/robot/subsystems/turret/commands/TurnTurret.java
@@ -2,14 +2,26 @@ package robot.subsystems.turret.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import robot.Robot;
+import robot.subsystems.turret.Turret.Direction;
+
+import static robot.Robot.turret;
 
 /**
  *
  */
 public class TurnTurret extends Command {
 
-    public TurnTurret() {
-        requires(Robot.turret);
+    private Direction direction;
+    private double angle;
+
+    public TurnTurret(double angle) {
+        this(angle, Direction.LEFT);
+    }
+
+    public TurnTurret(double angle, Direction direction) {
+        requires(turret);
+        this.angle = angle;
+        this.direction = direction;
     }
 
     // Called just before this Command runs the first time
@@ -20,12 +32,13 @@ public class TurnTurret extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        turret.setTargetAngle(angle);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return turret.getAngle() == angle;
     }
 
     // Called once after isFinished returns true
