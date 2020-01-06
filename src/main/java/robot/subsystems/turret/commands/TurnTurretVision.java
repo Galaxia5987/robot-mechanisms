ba42fcaba720753c0bd6c8a07caf7a80acbee277
Adam <adam.vichik@gmail.com>
2020-01-06 17:45:41 +0200

    Create TurnTurretVision to turn by vision angle

--- src/main/java/robot/subsystems/turret/commands/TurnTurretVision.java
+++ src/main/java/robot/subsystems/turret/commands/TurnTurretVision.java
@@ -1,4 +1,45 @@
 package robot.subsystems.turret.commands;
 
-public class TurnTurretVision {
+import edu.wpi.first.wpilibj.command.Command;
+import robot.subsystems.turret.ControlMode;
+
+import static robot.Constants.Turret.ANGLE_THRESHOLD;
+import static robot.Robot.turret;
+
+/**
+ *
+ */
+public class TurnTurretVision extends Command {
+
+    private double angle;
+
+    public TurnTurretVision() {
+        requires(turret);
+    }
+
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+        turret.setControlMode(ControlMode.ANGLE_CONTROL);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        this.angle = turret.getAngle() + turret.getVisionAngle();
+        turret.setTargetAngle(this.angle);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return Math.abs(turret.getAngle() - angle) <= ANGLE_THRESHOLD;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        //turret.stop();
+    }
 }
