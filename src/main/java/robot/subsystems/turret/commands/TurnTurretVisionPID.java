14c839c50275ced1ebf2d29aa03d56126e881670
Adam <adam.vichik@gmail.com>
2020-01-06 17:45:09 +0200

    Create TurnTurretVisionPID to operate straight on the motor

--- src/main/java/robot/subsystems/turret/commands/TurnTurretVisionPID.java
+++ src/main/java/robot/subsystems/turret/commands/TurnTurretVisionPID.java
@@ -1,4 +1,41 @@
 package robot.subsystems.turret.commands;
 
-public class TurnTurretVisionPID {
+import com.stormbots.MiniPID;
+import edu.wpi.first.wpilibj.command.Command;
+import robot.subsystems.turret.ControlMode;
+
+import static robot.Constants.VisionTurret.VISION_ANGLE_THRESHOLD;
+import static robot.Constants.VisionTurret.*;
+import static robot.Robot.turret;
+
+public class TurnTurretVisionPID extends Command {
+    private MiniPID anglePid = new MiniPID(VISION_KP, VISION_KI, VISION_KD);
+
+    public TurnTurretVisionPID() {
+        requires(turret);
+    }
+
+    @Override
+    protected void initialize() {
+        turret.setControlMode(ControlMode.SPEED_CONTROL);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        anglePid.setPID(VISION_KP, VISION_KI, VISION_KD);
+        turret.setSpeed(anglePid.getOutput(turret.getVisionAngle(), 0));
+    }
+
+    @Override
+    protected void end() {
+        turret.setTargetAngle(turret.getAngle());
+        turret.setControlMode(ControlMode.ANGLE_CONTROL);
+        turret.setSpeed(0);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return Math.abs(turret.getVisionAngle()) <= VISION_ANGLE_THRESHOLD;
+    }
 }
